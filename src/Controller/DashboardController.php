ba2a883943e2e564ee84fb883d358f2160b28bca
Amine Affif <affif.amine@live.fr>
2021-01-11 18:51:32 +0100

    finished the Dashboard page

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -19,29 +19,174 @@ class DashboardController extends AppController
      */
     public function index()
     {
-        $result = $this->loadModel('pokemons');
-        $data = $result->find('all');
+        // Retrieving data from DB
+        $resultAllPokemons = $this->loadModel('pokemons');
+        $pokeTypes = $this->loadModel('pokemon_types');
+        $resultAllSpeeds = $this->loadModel('pokemons');
+
+
         // If the actual pokemon is in the 4th generation
-        $data4th = $result->find('all')
+        $data4th = $resultAllPokemons->find('all')
             ->where(['id >=' => 387])
             ->where(['id <=' => 493]);
+
+        // If the actual pokemon is in the 1th generation
+        // And he's Fairy type
+        $dataPokeTypes1th = $pokeTypes->find('all')
+            // 1st generation
+            ->where(['pokemon_id >=' => 1])
+            ->where(['AND' => ['pokemon_id <=' => 151]])
+            ->where(['AND' => ['type_id =' => 10]]);
+        // If the actual pokemon is in the 3th generation
+        // And he's Fairy type
+        $dataPokeTypes3th = $pokeTypes->find('all')
+            // 1st generation
+            ->where(['pokemon_id >=' => 252])
+            ->where(['AND' => ['pokemon_id <=' => 386]])
+            ->where(['AND' => ['type_id =' => 10]]);
+        // If the actual pokemon is in the 7th generation
+        // And he's Fairy type
+        $dataPokeTypes7th = $pokeTypes->find('all')
+            // 1st generation
+            ->where(['pokemon_id >=' => 722])
+            ->where(['AND' => ['pokemon_id <=' => 809]])
+            ->where(['AND' => ['type_id =' => 10]]);
+
+        // Getting all the Pokemons
+        $dataAllSpeeds = $resultAllSpeeds->find('all');
+
+
+        $nBPokemon1th3th7th = 0;
+        $allTypes = array();
+
 
         // Initialisation variables
         $nBPokemon4th = 0;
         $allWeight = array();
         $avgWeight = 0;
 
+        $allFairyPokemons = array();
+
+        $fastestsPoke = array();
+
+        $nBPokemon1th3th7th = 0;
+        $length = 0;
+
+
         // Average weight
         foreach ($data4th as $row){
             $nBPokemon4th = $nBPokemon4th +1;
             $allWeight[] = $row->weight;
+        }
+        foreach ($dataAllSpeeds as $row){
+            $length+=1;
         }
         foreach ($allWeight as $val){
             $avgWeight = $avgWeight + $val;
         }
         $avgWeight = round($avgWeight/$nBPokemon4th, 2);
 
-        $this->set(compact('row', 'data', 'nBPokemon4th', 'allWeight', 'data4th', 'avgWeight'));
+
+        // Counting number of Fairy type Pokemon
+        foreach ($dataPokeTypes1th as $row){
+            $nBPokemon1th3th7th = $nBPokemon1th3th7th + 1;
+        }
+        foreach ($dataPokeTypes3th as $row){
+            $nBPokemon1th3th7th = $nBPokemon1th3th7th + 1;
+        }
+        foreach ($dataPokeTypes7th as $row){
+            $nBPokemon1th3th7th = $nBPokemon1th3th7th + 1;
+        }
+
+
+
+        // Filling array with string that forms '$speed $name'
+        foreach ($dataAllSpeeds as $row => $value){
+            $fastestsPoke[][] = $value->name;
+            $fastestsPoke[][] = $value->speed;
+            $pokemonSortedSpeed[] = $value->speed . ' ' . $value->name;
+        }
+
+
+
+        // Sorting by speed
+        sort($pokemonSortedSpeed, SORT_NUMERIC);
+        // Reverse Array
+        $reversed = array_reverse($pokemonSortedSpeed);
+
+        // displaying $pokemonSortedSpeed REVERSED array
+//        echo '<pre>';
+//        for ($i=0; $i < 10; $i++){
+//            echo sprintf("<li>%s</li>", $reversed[$i]);
+//        }
+//        echo '</pre>';
+
+//        echo '<pre>';
+//        print_r($reversed);
+//        echo '</pre>';
+
+        // Split the $speed . $name
+        $pattern = "/[\s]/";
+        foreach ($reversed as $row => $value){
+            $speedNameTab[] = preg_split($pattern, $value);
+//            echo '<pre>';
+//            print_r($speedNameTab[$row]);
+//            echo '</pre>';
+        }
+
+        //display Final array with speed for each pokemon sorted
+//        foreach ($speedNameTab as $row => $value){
+//            echo '<pre>';
+//            print_r($speedNameTab[$row]);
+//            echo '</pre>';
+//        }
+
+
+
+
+
+
+//      Walking Backwards in the speed sorted pokemons array
+        $index = count($pokemonSortedSpeed);
+
+//      displaying $pokemonSortedSpeed array backwards With while loop
+
+
+//        while($index) {
+//            echo sprintf("<li>%s</li>", $pokemonSortedSpeed[--$index]);
+//            $reversed[] = $pokemonSortedSpeed[--$index];
+//            if ($index == 856){
+//
+//            }
+//            else{
+//                break;
+//            }
+//        }
+
+//      displaying $pokemonSortedSpeed array backwards With for loop
+//        $reversed = array();
+//        for ($i = $index; $i<= $index + 9 && $i >0; $i--){
+//            echo sprintf("<li>%s</li>", $pokemonSortedSpeed[--$index]);
+//            $reversed[] = $pokemonSortedSpeed[--$index];
+//        }
+//        var_dump($reversed);
+
+
+
+
+
+
+
+        $this->set(compact('row',
+            'nBPokemon4th',
+            'allWeight',
+            'data4th',
+            'avgWeight',
+            'allFairyPokemons',
+            'nBPokemon1th3th7th',
+            'dataPokeTypes1th',
+            'speedNameTab',
+        ));
     }
 
     /**
--- templates/Dashboard/index.php
+++ templates/Dashboard/index.php
@@ -7,10 +7,55 @@
  * @var \App\Model\Entity\Dashboard $allWeight
  * @var \App\Model\Entity\Dashboard $data4th
  * @var \App\Model\Entity\Dashboard $avgWeight
+ * @var \App\Model\Entity\Dashboard $allFairyPokemons
+ * @var \App\Model\Entity\Dashboard $nBPokemon1th3th7th
+ * @var \App\Model\Entity\Dashboard $speedNameTab
  */
 ?>
 
 
+<div class="container2">
+    <div class="container-top2">
+        <!--AVG Weight-->
+        <div class="left2">
+            <h2 style="text-decoration: underline; margin-top: 30px">Poids moyen - 4ème génération</h2>
+            <h4>Pour les <?= $nBPokemon4th ?> Pokémons de la 4ème générations, le poids moyen des Pokémons est <strong><?= $avgWeight ?></strong>.</h4>
+        </div>
+        <!--Nb Pokemon Fairy type-->
+        <div class="left2">
+            <h2 style="text-decoration: underline; margin-top: 30px">Pokémons de type fée - 1ere, 3ème, 7ème génération</h2>
+            <h4>Parmis les générations 1, 3 et 7 il y a <strong><?= $nBPokemon1th3th7th ?></strong> Pokémons de type fée.</h4>
+        </div>
+    </div>
+</div>
 
-<h1>Pour les <?= $nBPokemon4th ?> Pokémons de la 4ème générations, le poids moyen des Pokémons est <strong><?= $avgWeight ?></strong></h1>
 
+<!--TOP 10-->
+<div class="container2">
+    <div class="container-top2">
+        <h2 style="margin-top: 30px;text-decoration: underline">Top 10 des Pokémon les plus rapides :</h2>
+
+        <div class="divTable">
+            <div class="divTableBody">
+                <div class="divTableRow">
+                    <div class="divTableCell" style="font-weight: bold; font-size: 1.25em">Classement</div>
+                    <div class="divTableCell" style="font-weight: bold; font-size: 1.25em">Pokémon</div>
+                    <div class="divTableCell" style="font-weight: bold; font-size: 1.25em">Speed</div>
+                </div>
+                <?php $increment = 0 ?>
+                <?php foreach ($speedNameTab as $row => $value) : ?>
+                    <?php if ($row < 10) : ?>
+                        <?php $increment += 1 ?>
+                        <div class="divTableRow">
+                            <div class="divTableCell"><?= $increment ?></div>
+                            <div class="divTableCell"><?= $value[1] ?></div>
+                            <div class="divTableCell"><?= $value[0] ?></div>
+                        </div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </div>
+        </div>
+
+
+    </div>
+</div>
